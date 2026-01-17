a67d2e211ef64b143b3a140b3c08aea08201ef16
Keran Yang <yangkr920208@gmail.com>
2026-01-17 00:54:03 -0500

    fix: expect should say what is expected
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Start a thread to listen on a local port.
     let _listener = TcpListener::bind("127.0.0.1:1234");
     // Connect a stream to the local port.
-    let stream = TcpStream::connect("127.0.0.1:1234").expect("Cannot connect to the server");
+    let stream =
+        TcpStream::connect("127.0.0.1:1234").expect("should be able to connect to the local port.");
     println!(
         "Connected! Local IP is {}",
         stream.local_addr().unwrap().ip()
