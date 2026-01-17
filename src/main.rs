25a3d1e0ac8b4209f5f50bebdf779def7061426f
Keran Yang <yangkr920208@gmail.com>
2026-01-17 00:50:48 -0500

    chore: can connect to a TCP local port
    Rust is hard. It took me almost 1h to finish this one without AI's help.
    
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::error::Error;
+use std::net::{TcpListener, TcpStream};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // Start a thread to listen on a local port.
+    let _listener = TcpListener::bind("127.0.0.1:1234");
+    // Connect a stream to the local port.
+    let stream = TcpStream::connect("127.0.0.1:1234").expect("Cannot connect to the server");
+    println!(
+        "Connected! Local IP is {}",
+        stream.local_addr().unwrap().ip()
+    );
+    Ok(())
+}
