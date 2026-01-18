0c62657168415835bb8748fe357488e6cc2dd4dc
Keran Yang <yangkr920208@gmail.com>
2026-01-18 06:58:56 -0500

    chore: both server and client start
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
 use std::error::Error;
-use std::net::{TcpListener, TcpStream};
+use std::net::TcpListener;
+use std::thread;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Start a thread to listen on a local port.
+    // Start a listening on a local port.
     let _listener = TcpListener::bind("127.0.0.1:1234");
+
+    // Start a thread as server.
+    // use std::thread;
+    let _server = thread::spawn(|| {
+        run_server();
+    });
+
+    // Start a thread as client.
+    let _client = thread::spawn(|| {
+        run_client();
+    });
+
+    // Note: this is a long running application.
+    // TODO - implement graceful shutdown.
+
     // Connect a stream to the local port.
-    let stream =
-        TcpStream::connect("127.0.0.1:1234").expect("should be able to connect to the local port.");
-    println!(
-        "Connected! Local IP is {}",
-        stream.local_addr().unwrap().ip()
-    );
+    // let stream =
+    //     TcpStream::connect("127.0.0.1:1234").expect("should be able to connect to the local port.");
+    // println!(
+    //     "Connected! Local IP is {}",
+    //     stream.local_addr().unwrap().ip()
+    // );
     Ok(())
+}
+
+fn run_server() {
+    println!("The server started.")
+}
+
+fn run_client() {
+    println!("The client started.")
 }
