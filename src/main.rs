e4308cf0e69c002ddfa3477b06b2534614ee0e52
Keran Yang <yangkr920208@gmail.com>
2026-01-18 08:28:37 -0500

    feat: can chat
    Signed-off-by: Keran Yang <yangkr920208@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,47 @@
 use std::error::Error;
-use std::net::TcpListener;
+use std::io::Read;
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc;
 use std::thread;
+
+const MAX_MSG_SIZE: usize = 1000;
+
+enum Message {
+    ConnectionEstablished(TcpStream),
+    ConnectionClosed(TcpStream),
+    NewMessage(String),
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Start a listening on a local port.
-    let _listener = TcpListener::bind("127.0.0.1:1234");
+    let listener = TcpListener::bind("127.0.0.1:1234").unwrap();
+
+    // Create a channel for message passing between clients and server.
+    // TODO - I think the <Message> can be removed once, since rust an automatically infer the type.
+    let (sender, receiver) = mpsc::channel::<Message>();
 
     // Start a thread as server.
     // use std::thread;
     let _server = thread::spawn(|| {
-        run_server();
+        run_server(receiver);
     });
 
-    // Start a thread as client.
-    let _client = thread::spawn(|| {
-        run_client();
-    });
+    // Accept connections, for each connection, start a separate client thread
+    loop {
+        match listener.accept() {
+            Ok((stream, _)) => {
+                let tx = sender.clone();
+                // Start a thread as client.
+                let _client = thread::spawn(|| {
+                    run_client(stream, tx);
+                });
+            }
+            Err(e) => {
+                eprint!("ERROR: accepting connections from a client {e}");
+                break;
+            }
+        }
+    }
 
     // Note: this is a long running application.
     // TODO - implement graceful shutdown.
@@ -30,10 +56,52 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn run_server() {
-    println!("The server started.")
+fn run_server(rx: mpsc::Receiver<Message>) {
+    println!("The server thread started.");
+
+    // Handle incoming Messages.
+    loop {
+        let msg = rx.recv().expect("should receive a message.");
+        match msg {
+            Message::ConnectionEstablished(_) => {
+                println!("A connection from a client is established.")
+            }
+            Message::ConnectionClosed(_) => {
+                println!("A connection from a client is closed.")
+            }
+            Message::NewMessage(client_msg) => {
+                println!("Received a message: {}", client_msg);
+            }
+        }
+    }
 }
 
-fn run_client() {
-    println!("The client started.")
+fn run_client(mut stream: TcpStream, tx: mpsc::Sender<Message>) {
+    // Accepting Inputs
+    loop {
+        let mut buf = [0; MAX_MSG_SIZE];
+        let read_res = stream.read(&mut buf);
+        match read_res {
+            Ok(_) => {
+                // TODO - Error Handling.
+                let content_res = String::from_utf8(buf.to_vec());
+                match content_res {
+                    Ok(c) => {
+                        let _send_res = tx.send(Message::NewMessage(c));
+                    }
+                    Err(e) => {
+                        eprint!("ERROR: reading inputs, closing the connection. {e}");
+                        let _send_res = tx.send(Message::ConnectionClosed(stream));
+                        break;
+                    }
+                }
+            }
+            Err(e) => {
+                eprint!("ERROR: reading inputs, closing the connection. {e}");
+                // TODO - Error Handling.
+                let _send_res = tx.send(Message::ConnectionClosed(stream));
+                break;
+            }
+        }
+    }
 }
